e32c443585f8d9feae0351ce8e20ed234c22409f
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-05-04 16:12:09 -1000

    Update and rename university.cpp to parking.cpp

---
+++ parking.cpp
@@ -0,0 +1,20 @@
+/*
+Design and implement a simple command-line Parking Lot Management System in C++ to
+register cars entering a parking lot, track their information, and persist the data to a file for
+future use.
+
+Your code should be able to:
+  - Add a new car
+  - Remove a car by license plate
+  - Search a car by license plate
+  - View all currently parked cars
+  - Quit the application
+
+Car's information has: License plate: 6 alphanumeric characters; First/last name: letters only;
+                       Permit ID: 1 letter + 8 digits (total 9 characters); Time: valid 24-hour time (HH:MM)
+
+  Data is saved in a file named parking_data.txt
+
+  See PDF for sample input/output
+
+*/
